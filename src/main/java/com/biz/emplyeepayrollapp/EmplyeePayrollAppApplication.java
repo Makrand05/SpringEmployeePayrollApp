df8445882d45ad326cc28a0ca284048b43dd685b
Makrand <makrandshingare@gmail.com>
2022-04-12 08:41:29 +0530

    Added : Created the Employee Payroll app setup

---
+++ src/main/java/com/biz/emplyeepayrollapp/EmplyeePayrollAppApplication.java
@@ -0,0 +1,14 @@
+package com.biz.emplyeepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmplyeePayrollAppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EmplyeePayrollAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/biz/emplyeepayrollapp/EmplyeePayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biz.emplyeepayrollapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmplyeePayrollAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
